033027f9683ee2bec11dc0d82b0408cd654e4ec1
RasPelikan <RasPelikan@users.noreply.github.com>
2015-10-31 21:20:17 +0100

    Added 'rebuild' if tsc-config changed

--- com.raspelikan.m2econnectors.frontend_typecs/src/main/java/com/raspelikan/m2econnectors/frontend/TypecsPluginConfigurator.java
+++ com.raspelikan.m2econnectors.frontend_typecs/src/main/java/com/raspelikan/m2econnectors/frontend/TypecsPluginConfigurator.java
@@ -5,6 +5,8 @@ import java.util.logging.Logger;
 
 import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.project.MavenProject;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -16,13 +18,13 @@ import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 import org.eclipse.m2e.jdt.IClasspathDescriptor;
 import org.eclipse.m2e.jdt.IJavaProjectConfigurator;
 
+import com.axmor.eclipse.typescript.builder.builder.TypescriptBuilder;
 import com.axmor.eclipse.typescript.builder.builder.TypescriptNature;
 import com.axmor.eclipse.typescript.core.TypeScriptCompilerSettings;
 
 public class TypecsPluginConfigurator extends AbstractProjectConfigurator
 		implements IJavaProjectConfigurator {
 
-	@SuppressWarnings("unused")
 	private static final Logger logger = Logger
 			.getLogger(TypecsPluginConfigurator.class.getCanonicalName());
 
@@ -46,24 +48,42 @@ public class TypecsPluginConfigurator extends AbstractProjectConfigurator
 	public void configure(ProjectConfigurationRequest request,
 			IProgressMonitor monitor) throws CoreException {
 		
+		final IProject project = request.getProject();
+		
 		// add TypEcs's project-nature
-		addNature(request.getProject(), TypescriptNature.NATURE_ID, monitor);
+		
+		addNature(project, TypescriptNature.NATURE_ID, monitor);
 		
 		// configure TypEcs-plugin
-		setTypescriptCompilerSettings(request, monitor);
+		// load current TypEcs settings
+		
+		final boolean rebuild = setTypescriptCompilerSettings(
+					request, monitor);
+
+		// rebuild project if necessary
+		
+		if (rebuild) {
+			
+			logger.info("Rebuilding typescript-files...");
+			
+			project.build(IncrementalProjectBuilder.FULL_BUILD,
+					TypescriptBuilder.BUILDER_ID, null, monitor);
+			
+		}
 
 	}
 
-	private void setTypescriptCompilerSettings(ProjectConfigurationRequest request,
+	private boolean setTypescriptCompilerSettings(ProjectConfigurationRequest request,
 			IProgressMonitor monitor) throws CoreException {
-
-		// load current TypEcs settings
-		TypeScriptCompilerSettings settings = TypeScriptCompilerSettings
-				.load(request.getProject());
 
 		final MavenProject project = request.getMavenProject();
 		final IMavenProjectFacade mavenProjectFacade = request
 				.getMavenProjectFacade();
+		
+		final TypeScriptCompilerSettings settings = TypeScriptCompilerSettings
+				.load(request.getProject());
+		
+		boolean rebuild = false;
 		
 		// process all executions this configuration is called for
 		final List<MojoExecution> mojoExecutions = this.getMojoExecutions(request, monitor);
@@ -71,8 +91,14 @@ public class TypecsPluginConfigurator extends AbstractProjectConfigurator
 			
 			// read srcDir
 			
-			final String srcDir = maven.getMojoParameterValue(
+			String srcDir = maven.getMojoParameterValue(
 					project, mojoExecution, "srcDir", String.class, monitor);
+			
+			final String projectPath = request.getProject().getLocation().toString();
+			if (srcDir.startsWith(projectPath)) {
+				srcDir = srcDir.substring(projectPath.length(), srcDir.length());
+			}
+			
 			final IPath projectRelativePathSrcDir = mavenProjectFacade
 					.getProject().getFolder(srcDir).getProjectRelativePath();
 			if (projectRelativePathSrcDir == null) {
@@ -83,18 +109,31 @@ public class TypecsPluginConfigurator extends AbstractProjectConfigurator
 				throw new CoreException(status);
 			}
 			
-			settings.setSource(projectRelativePathSrcDir.toString());
+			String srcDirRelative = projectRelativePathSrcDir.toString();
+			if ((settings.getSource() == null)
+					|| !settings.getSource().equals(srcDirRelative)) {
+				rebuild = true;
+			}
+			settings.setSource(srcDirRelative);
 			
 			// read preserveDirectoryStructure
 			
 			final boolean preserveDirectoryStructure = maven.getMojoParameterValue(
 					project, mojoExecution, "preserveDirectoryStructure", Boolean.class, monitor);
+			if (settings.isTargetRelativePathBasedOnSource() != preserveDirectoryStructure) {
+				rebuild = true;
+			}
 			settings.setTargetRelativePathBasedOnSource(preserveDirectoryStructure);
 			
 			// read outDir
 			
-			final String outDir = maven.getMojoParameterValue(
+			String outDir = maven.getMojoParameterValue(
 					project, mojoExecution, "outDir", String.class, monitor);
+			
+			if (outDir.startsWith(projectPath)) {
+				outDir = outDir.substring(projectPath.length(), outDir.length());
+			}
+			
 			final IPath projectRelativePathOutDir = mavenProjectFacade
 					.getProject().getFolder(outDir).getProjectRelativePath();
 			if (projectRelativePathOutDir == null) {
@@ -105,15 +144,27 @@ public class TypecsPluginConfigurator extends AbstractProjectConfigurator
 				throw new CoreException(status);
 			}
 			
-			settings.setTarget(projectRelativePathOutDir.toString());
+			String outDirRelative = projectRelativePathOutDir.toString();
+			if ((settings.getTarget() == null)
+					|| !settings.getTarget().equals(outDirRelative)) {
+				rebuild = true;
+			}
+			settings.setTarget(outDir);
 			
 			// mapRoot
 			
 			final String mapRoot = maven.getMojoParameterValue(
 					project, mojoExecution, "mapRoot", String.class, monitor);
 			if (mapRoot != null) {
+				if ((settings.getMapRoot() == null)
+						|| !settings.getMapRoot().equals(mapRoot)) {
+					rebuild = true;
+				}
 				settings.setMapRoot(mapRoot);
 			} else {
+				if (!settings.getMapRoot().equals("")) {
+					rebuild = true;
+				}
 				settings.setMapRoot("");
 			}
 			
@@ -121,30 +172,45 @@ public class TypecsPluginConfigurator extends AbstractProjectConfigurator
 			
 			final boolean noResolve = maven.getMojoParameterValue(
 					project, mojoExecution, "noResolve", Boolean.class, monitor);
+			if (noResolve != settings.isNoResolve()) {
+				rebuild = true;
+			}
 			settings.setNoResolve(noResolve);
 			
 			// noImplicitAny
 
 			final boolean noImplicitAny = maven.getMojoParameterValue(
 					project, mojoExecution, "noImplicitAny", Boolean.class, monitor);
+			if (noImplicitAny != settings.isNoImplicitAny()) {
+				rebuild = true;
+			}
 			settings.setNoImplicitAny(noImplicitAny);
 			
 			// declaration
 			
 			final boolean declaration = maven.getMojoParameterValue(
 					project, mojoExecution, "declaration", Boolean.class, monitor);
+			if (declaration != settings.isGenerateDeclaration()) {
+				rebuild = true;
+			}
 			settings.setGenerateDeclaration(declaration);
 			
 			// sourceMap
 			
 			final boolean sourceMap = maven.getMojoParameterValue(
 					project, mojoExecution, "sourceMap", Boolean.class, monitor);
+			if (sourceMap != settings.isSourceMap()) {
+				rebuild = true;
+			}
 			settings.setSourceMap(sourceMap);
 			
 			// removeComments
 			
 			final boolean removeComments = maven.getMojoParameterValue(
 					project, mojoExecution, "removeComments", Boolean.class, monitor);
+			if (removeComments != settings.isRemoveComments()) {
+				rebuild = true;
+			}
 			settings.setRemoveComments(removeComments);
 			
 			// module
@@ -152,8 +218,16 @@ public class TypecsPluginConfigurator extends AbstractProjectConfigurator
 			final String module = maven.getMojoParameterValue(
 					project, mojoExecution, "module", String.class, monitor);
 			if (module != null) {
+				if ((settings.getModule() == null)
+						|| !module.equals(settings.getModule())) {
+					rebuild = true;
+				}
 				settings.setModule(module);
 			} else {
+				if ((settings.getModule() == null)
+						|| !"ES3".equals(settings.getModule())) {
+					rebuild = true;
+				}
 				settings.setModule("ES3");
 			}
 			
@@ -162,14 +236,24 @@ public class TypecsPluginConfigurator extends AbstractProjectConfigurator
 			final String target = maven.getMojoParameterValue(
 					project, mojoExecution, "target", String.class, monitor);
 			if (target != null) {
+				if ((settings.getTargetVersion() == null)
+						|| !target.equals(settings.getTargetVersion())) {
+					rebuild = true;
+				}
 				settings.setTargetVersion(target);
 			} else {
-				settings.setTargetVersion("system");
+				if ((settings.getTargetVersion() == null)
+						|| !"default".equals(settings.getTargetVersion())) {
+					rebuild = true;
+				}
+				settings.setTargetVersion("default");
 			}
-
+			
+			settings.save();
+			
 		}
 		
-		settings.save();
+		return rebuild;
 
 	}
 
